405aae68fa25f20a2d47c63019db8f6fe1491914
Anwar Jahid <ajr.jahid@gmail.com>
2022-03-28 10:10:39 +0600

    first commit

---
+++ src/AutoLogin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Anwar\AutoLogin;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AutoLogin
+{
+    // Build your next great package.
+    public function generateToken($email, $password){
+        $user = \DB::table(config('autologin.users_table', 'users'))->where('email', $email);
+        $userData = $user->first();
+        if ($user && Hash::check($password, $userData->password)){
+            $user->update([
+                'app_token' => Str::random(60),
+                'app_reference' => request()->url(),
+            ]);
+            return response()->json($user->first());
+        }
+        return response()->json([
+            'status' => false,
+            'message' => "Check your email and password."
+        ],403);
+    }
+}
---
+++ src/Controllers/AutoLoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Anwar\AutoLogin\Controllers;
+
+use Anwar\AutoLogin\AutoLogin;
+use App\Http\Traits\AuthorizesRequests;
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class AutoLoginController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function generateToken(Request $request){
+        $validate = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if ($validate->fails()) {
+            return [
+                'status' => false,
+                'error' => $validate->errors(),
+            ];
+        }
+        $email =$request->email;
+        $password = $request->password;
+        $autologin = new AutoLogin();
+        return $autologin->generateToken($email, $password);
+    }
+
+    public function autoLogin(Request $request){
+        $validate = Validator::make($request->all(), [
+            'app_token' => 'required',
+        ]);
+
+        if ($validate->fails()) {
+            return [
+                'status' => false,
+                'error' => $validate->errors(),
+            ];
+        }
+
+        $app_token = $request->app_token;
+        $user = User::where('app_token', $app_token)
+//            ->where('app_reference', $request->url())
+            ->first();
+        if ($user) {
+            $u = auth()->login($user);
+            return redirect('dashboard');
+        }else{
+            return [
+                'status' => false,
+                'error' => "Login Failed",
+            ];
+        }
+    }
+}
